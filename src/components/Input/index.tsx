047449c413c2d707e57bcabd1bbd33fcd37adab6
CarinaRoberta <silva.carina@live.com>
2022-01-21 22:10:16 -0300

    add input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,63 @@
-import React, { InputHTMLAttributes } from "react";
+import React, { InputHTMLAttributes, useState } from "react";
 import { Container } from "./style";
 
 export interface InputProps {
-  label: string;
+  label?: string;
   typeInput: string;
+  idInput: string;
+  nameInput: string;
+  valueInput: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
 }
 
-export const Input: React.FC<InputProps> = ({ label, typeInput, ...props }) => {
+export const Input: React.FC<InputProps> = ({
+  label,
+  typeInput,
+  idInput,
+  nameInput,
+  valueInput,
+  onChange,
+  placeholder,
+  ...props
+}) => {
   return (
     <Container>
-      <label>{label}</label>
-      <input type={typeInput || "text"} {...props} />
+      {label ? <label htmlFor={idInput}>{label || "Label"}</label> : ""}
+      <input
+        placeholder={placeholder}
+        type={typeInput || "text"}
+        id={idInput}
+        name={nameInput}
+        value={valueInput}
+        onChange={onChange}
+        {...props}
+      />
+    </Container>
+  );
+};
+
+export const InputWithoutLabel: React.FC<InputProps> = ({
+  label,
+  typeInput,
+  idInput,
+  nameInput,
+  valueInput,
+  onChange,
+  placeholder,
+  ...props
+}) => {
+  return (
+    <Container>
+      <input
+        placeholder={placeholder}
+        type={typeInput || "text"}
+        id={idInput}
+        name={nameInput}
+        value={valueInput}
+        onChange={onChange}
+        {...props}
+      />
     </Container>
   );
 };
